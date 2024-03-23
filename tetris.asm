b5ac57731e3964a5d3bd646c89c29c9ae6059ff8
Dharma <dharma.ong@mail.utoronto.ca>
2024-03-23 16:53:43 -0400

    added grid and playing field

--- tetris.asm
+++ tetris.asm
@@ -46,7 +46,7 @@ main:
     add $t3, $zero, $zero #set index value ($t3) to zero
     
     li $t9, 0
-    li $t8, 96
+    li $t8, 256
     add $t5, $t0, $t9
     
     j init_board
@@ -58,8 +58,9 @@ main:
     # Initialize the game
     li $t1, 0x525252            # set $t1 to grey
     
-init_board:
-        beq $t9, 1008, draw_border
+    
+init_board:                             # draw a grid across the entire screen
+        beq $t9, 4096, draw_border      # once the entire grid is drawn, jump to drawing the playing box
         
         div $t9, $t8
         mfhi $t7
@@ -67,11 +68,11 @@ init_board:
         li $t3, 0
         
         beq $t7, 0, draw_even_row
-        beq $t7, 48, draw_odd_row
+        beq $t7, 128, draw_odd_row
         
         j exit
         
-    end_init_board:
+end_init_board:
         
     draw_even_row:                          # starts with gray two
         sw $t2, 0($t5)
@@ -81,7 +82,7 @@ init_board:
         
         addi $t3, $t3, 4
         addi $t9, $t9, 8
-        beq $t3, 24 init_board
+        beq $t3, 64 init_board
         
         j draw_even_row
         
@@ -91,18 +92,22 @@ init_board:
         sw $t2, 0($t5)
         addi $t5, $t5, 4
         
+        
         addi $t3, $t3, 4
         addi $t9, $t9, 8
-        beq $t3, 24, init_board
+        beq $t3, 64, init_board
         
         j draw_odd_row
 
 draw_border:
     
-    addi $a0, $zero, 21	# height of display
-    addi $a1, $zero, 12	# width of display
-    add $t0, $zero, $zero
     li $t1, 0xa6a6a6     
+    sw $t1, 776($t0)
+    
+    addi $a0, $zero, 21	# height of display
+    addi $a1, $zero, 11	# width of display
+    add $t0, $zero, $zero
+    
     add $t2, $zero, $zero
     add $t3, $zero, $zero
     add $t5, $zero, $zero
@@ -112,9 +117,7 @@ draw_border:
     
     init_h:
         lw $t0, ADDR_DSPL           # $t0 = base address for display
-        subi $t6, $a0, 1
-        mul $t5, $t6, 48
-        add $t0, $t0, $t5
+        addi $t0, $t0, 3336         # move to the first byte where we want to draw the box
 
     draw_h:
         sw $t1, 0($t0)              # paint $t0 grey
@@ -126,24 +129,90 @@ draw_border:
         
     init_v:
         lw $t0, ADDR_DSPL           # $t0 = base address for display
+        addi $t0, $t0, 776
         
     draw_v:
         sw $t1, 0($t0)              # paint $t0 grey
-        addi $t0, $t0, 48           # move to next pixel
+        addi $t0, $t0, 128          # move to next pixel (downwards)
         addi $t2, $t2, 1            # increment counter
         
         beq $t2, $a0, up_v
         
         mul $t8, $a0, 2
         
-        beq $t2, $t8, exit
+        beq $t2, $t8, cover_background
         j draw_v
         
         
     up_v:
         lw $t0, ADDR_DSPL 
-        addi $t0, $t0, 44
+        addi $t0, $t0, 820          # go to the address for the top right of the box
         j draw_v
+        
+cover_background: 
+    li $t1, 0x000000            # use a black color for the background
+    init_draw_upper_sec: 
+        lw $t0, ADDR_DSPL           # go back to the top  left corner of bitmap display     
+        li $t2, 0                   # incrementer variable
+    
+    draw_upper_sec:
+        beq $t2, 192, init_draw_lower_sec
+        sw $t1, 0($t0)
+        add $t0, $t0, 4
+        addi $t2, $t2, 1
+        j draw_upper_sec
+    
+    init_draw_lower_sec:
+        lw $t0, ADDR_DSPL
+        addi $t0, $t0, 3456
+        li $t2, 0       
+        
+    draw_lower_sec:
+        beq $t2, 160, init_draw_left_sec
+        sw $t1, 0($t0)
+        add $t0, $t0, 4
+        addi $t2, $t2, 1
+        j draw_lower_sec
+    
+    init_draw_left_sec:
+        lw $t0, ADDR_DSPL
+        addi $t0, $t0, 768
+        li $t2, 0
+        
+    draw_left_sec:
+        beq $t2, 21, init_draw_right_sec
+        sw $t1, 0($t0)
+        add $t0, $t0, 4
+        sw $t1, 0($t0)
+        add $t0, $t0, 124
+        
+        addi $t2, $t2, 1
+        j draw_left_sec
+        
+    init_draw_right_sec:
+        lw $t0, ADDR_DSPL
+        addi $t0, $t0, 768
+        li $t2, 0
+        # sw $t1, 0($t0)
+    
+    draw_right_sec:
+        beq $t2, 21, exit 
+        li $t3, 0
+        addi $t0, $t0, 56
+        addi $t2, $t2, 1
+        j draw_row_for_right_sec
+        
+        
+    
+    draw_row_for_right_sec:
+        beq $t3, 18, draw_right_sec
+        sw $t1, 0($t0)
+        addi $t0, $t0, 4
+        addi $t3, $t3, 1
+        j draw_row_for_right_sec
+        
+    
+end_cover_background: 
         
 game_loop:
 	# 1a. Check if key has been pressed
